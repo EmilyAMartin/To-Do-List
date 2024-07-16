b3bf32db6abbbcfeddb7dc6407ad4c174e7a7185
Emily Martin <163541100+EmilyAMartin@users.noreply.github.com>
2024-07-16 21:10:06 -0230

    Working on todo list application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+
+    //Add new task to list//
+    setList([...list, newTodo]);
+    //Clear input box//
+    setInput("");
+  };
+
+  const deleteTodo = (id) => {
+    const newList = list.filter((todo) => todo.id !== id);
+    setList(newList);
+  };
   return (
-    <div className="App">
-      <todowrapper/>
+    <div>
+      <div className="todo">
+        <h1>To Do List</h1>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={() => addTodo(input)}>Add</button>
+        <ul>
+          {list.map((todo) => (
+            <li key={todo.id}>
+              {todo.todo}
+              <button onClick={() => deleteTodo(todo.id)}>&times;</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
